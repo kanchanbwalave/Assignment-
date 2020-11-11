dab7ea8919f8ea25b2af08b5b03a93490efb81cb
kanchanbwalave <74292997+kanchanbwalave@users.noreply.github.com>
2020-11-11 14:56:22 +0530

    Day 1 assignment submitted

---
+++ Assignment_NO_1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Jumble to english word
+OBANWRI- RAINBOW
+
+
+# In[1]:
+
+
+#program to print letsupgrade
+name=input()
+print(name.upper())
+
+
+# In[ ]:
+
+
+# program to print profit or loss
+cost=int(input())
+selling=int(input())
+if selling>cost:
+    print("Profit")
+elif selling<cost:
+    print("Loss")
+else:
+    print("Neither")
+
+
+# In[6]:
+
+
+#Euros to rupee conversion
+Euros=int(input())
+Rupee=Euros*80
+print(Rupee)
+
+
+# In[ ]:
+
+
+
+
